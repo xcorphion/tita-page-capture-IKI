41b48f3a9ab6b25762ba2b4671c17587e3a5bca9
joaosollatori-dotcom <joaosollatori@gmail.com>
2026-05-07 14:35:51 -0500

    remoção de loop

--- Xcorphion_Platform/next.config.js
+++ Xcorphion_Platform/next.config.js
@@ -5,15 +5,7 @@ const nextConfig = {
   devIndicators: {
     appIsrStatus: false,
     buildActivity: false,
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/research/:path*',
-        destination: `${process.env.RESEARCH_URL || 'http://localhost:3001'}/research/:path*`,
-      },
-    ]
-  },
+  }
 }
 
 module.exports = nextConfig;
